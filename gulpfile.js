0d4400ae0863be54b05a5ad4ab06d1051e93506f
connorwolf <origamingofficial@gmail.com>
2018-08-01 00:49:06 +0100

    Release v0.5.0

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp"),
+	gitWatch = require("gulp-git-watch"),
+	git = require("gulp-git");
+
+gulp.task("git-watch", function() {
+	gitWatch()
+		.on("change", function(newHash, oldHash) {
+			console.log("Found repository change from ", oldHash, "->", newHash);
+			git.pull("origin", "master", (err) => {
+				if (err) console.error(err);
+				else console.log("Up to date.");
+			});
+		});
+});
